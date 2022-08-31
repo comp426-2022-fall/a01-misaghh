5a3dcf270c051c6601fb20ff6398993794313f2e
Misagh  Haghar <misaghh@email.unc.edu>
2022-08-31 14:10:31 -0400

    work plz

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // 3. end with the data that you are reading in from ./public/index.html.
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('content-type', 'text/html');
-    res.end('<h1>./public/index.html<h1>');
+    res.setHeader('content-type', `text/html`);
+    res.end(`./public/index.html`);
   });
 
 
